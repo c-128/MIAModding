27be1cefe8c42e13300e308aedd02e1a317f480a
NullPointerException <does.not@exist.com>
2022-12-13 08:19:19 +0100

    test

--- src/script.ts
+++ src/script.ts
@@ -8,4 +8,5 @@ Config.loadConfig();
 if (window.location.href.includes("einstellungen")) SettingsMod();
 if (Config.INSTANCE.darkmode.value) DarkmodeMod();
 if (Config.INSTANCE.showTodaysTask.value) ShowTodaysTaskMod();
-console.log(Config.INSTANCE)
+
+alert(1);
