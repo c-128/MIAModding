67ac07472b909d99ff214a26711eb3c0f1dbdd27
NullPointerException <does.not@exist.com>
2022-12-13 08:16:15 +0100

    Massive reworkings

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## Download instructions:
 1. You need a browser capable of running user.js files (e.g. Firefox with the Greasemonkey extension or Kiwi Browser)
-2. Download `index.user.js` [here](https://github.com/c-128/MIAModding/raw/main/index.user.js)
-3. In your extension menu, add `index.user.js`
+2. Download `miapp.user.js` [here](https://github.com/c-128/MIAModding/raw/main/miapp.user.js)
+3. In your extension menu, add `miapp.user.js`
---
+++ miapp.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         MIA++
+// @version      0.1
+// @description  Mathe im Advent Modding
+// @author       .idea
+// @match        https://www.mathe-im-advent.de/de/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @require      https://raw.githubusercontent.com/c-128/MIAModding/main/script.js
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Your code here...
+})();
